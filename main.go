b610b68e12b5e9dd8dae421c331c8ecaf06c8913
banahaker <pn0818x@outlook.com>
2024-04-21 13:36:29 +0800

    feat: create a cli with urfave/cli

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-  fmt.Println("Hello, world!")
+	app := &cli.App{
+		Name:     "component-manager",
+		HelpName: "cm",
+		Usage:    "an component manager",
+		Version:  "v0.0.0",
+		Commands: []*cli.Command{
+			{
+				Name:    "version",
+				Aliases: []string{"v"},
+				Usage:   "show the version of ui",
+				Action: func(ctx *cli.Context) error {
+					fmt.Println(ctx.App.Version)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
