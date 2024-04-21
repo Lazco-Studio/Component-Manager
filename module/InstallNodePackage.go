0c84dc27f0d6342a8a1c21b6897d4a7791515e7a
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-21 21:05:35 +0800

    feat: :sparkles: create function for installing node packages

---
+++ module/InstallNodePackage.go
@@ -0,0 +1,24 @@
+package module
+
+import (
+	"os"
+	// "bytes"
+	// "errors"
+	"os/exec"
+)
+
+func InstallNodePackage(absolutePath string, packageManager string, isDevDependency bool, packageName string) error {
+	cmd := exec.Command(packageManager, "install", packageName, DevDependencyArg(isDevDependency))
+	cmd.Dir = absolutePath
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	return err
+}
+
+func DevDependencyArg(devDependency bool) string {
+	if devDependency {
+		return "-D"
+	}
+	return ""
+}
