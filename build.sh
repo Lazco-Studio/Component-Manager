aedfc724cb627e62838fea67036ef1a7f7df5e5d
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-22 11:46:34 +0800

    build: :sparkles: add build script for linux

---
+++ build.sh
@@ -0,0 +1,5 @@
+GITHUB_TOKEN=$(cat env/GITHUB_TOKEN)
+
+go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/cm-cli_linux_amd64_uncompressed
+
+upx -f --best --lzma ./dist/cm-cli_linux_amd64_uncompressed -o ./dist/cm-cli_linux_amd64
