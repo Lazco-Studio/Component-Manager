d5def72f52c9579389cbaa9a15701a438654d0cf
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-25 11:49:26 +0800

    v0.2.24

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 		Name:     "component-manager",
 		HelpName: "cm",
 		Usage:    "an component manager",
-		Version:  "v0.2.23",
+		Version:  "v0.2.24",
 		Commands: []*cli.Command{
 			{
 				Name:    "version",
