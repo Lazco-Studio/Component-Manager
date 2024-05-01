f84b1ecfb6660a839b88eca1a6ec31fcf73fe440
OnCloud <anonymousaaaa41414141@gmail.com>
2024-05-01 14:26:26 +0800

    build: :white_check_mark: add option to specify build architecture

--- build.sh
+++ build.sh
@@ -28,32 +28,48 @@ fi
 mkdir -p dist/uncompressed
 mkdir -p dist/compressed
 
-# Build binaries
-echo -e "${MAGENTA}Building binaries...${NC}"
-# linux/amd64
-echo -e "${MAGENTA}${ROCKET} Processing ${BLUE}linux/amd64${MAGENTA}...${NC}"
-CGO_ENABLED=0  GOOS=linux    GOARCH=amd64 go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/uncompressed/cm-cli_linux_amd64
-echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/uncompressed/cm-cli_linux_amd64${NC}"
-# darwin/amd64
-echo -e "${MAGENTA}${ROCKET} Processing ${BLUE}darwin/amd64${MAGENTA}...${NC}"
-CGO_ENABLED=0  GOOS=darwin   GOARCH=amd64 go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/uncompressed/cm-cli_darwin_amd64
-echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/uncompressed/cm-cli_darwin_amd64${NC}"
-# windows/amd64
-echo -e "${MAGENTA}${ROCKET} Processing ${BLUE}windows/amd64${MAGENTA}...${NC}"
-CGO_ENABLED=0  GOOS=windows  GOARCH=amd64 go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/uncompressed/cm-cli_windows_amd64
-echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/uncompressed/cm-cli_windows_amd64${NC}"
+function linux_amd64() {
+  echo -e "${MAGENTA}${ROCKET} Building ${BLUE}linux/amd64${MAGENTA}...${NC}"
+  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/uncompressed/cm-cli_linux_amd64
+  echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/uncompressed/cm-cli_linux_amd64${NC}"
 
-# Compress binaries
-echo -e "${MAGENTA}Compressing binaries...${NC}"
-# linux/amd64
-echo -e "${MAGENTA}${ROCKET} Processing ${BLUE}linux/amd64${MAGENTA}...${NC}"
-upx -f --best --lzma ./dist/uncompressed/cm-cli_linux_amd64 -o ./dist/compressed/cm-cli_linux_amd64
-echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/compressed/cm-cli_linux_amd64${NC}"
-# darwin/amd64
-echo -e "${MAGENTA}${ROCKET} Processing ${BLUE}darwin/amd64${MAGENTA}...${NC}"
-upx -f --best --lzma ./dist/uncompressed/cm-cli_darwin_amd64 -o ./dist/compressed/cm-cli_darwin_amd64 --force-macos
-echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/compressed/cm-cli_darwin_amd64${NC}"
-# windows/amd64
-echo -e "${MAGENTA}${ROCKET} Processing ${BLUE}windows/amd64${MAGENTA}...${NC}"
-upx -f --best --lzma ./dist/uncompressed/cm-cli_windows_amd64 -o ./dist/compressed/cm-cli_windows_amd64
-echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/compressed/cm-cli_windows_amd64${NC}"
+  echo -e "${MAGENTA}${ROCKET} Compressing ${BLUE}linux/amd64${MAGENTA}...${NC}"
+  upx -f --best --lzma ./dist/uncompressed/cm-cli_linux_amd64 -o ./dist/compressed/cm-cli_linux_amd64
+  echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/compressed/cm-cli_linux_amd64${NC}"
+}
+
+function darwin_amd64() {
+  echo -e "${MAGENTA}${ROCKET} Building ${BLUE}darwin/amd64${MAGENTA}...${NC}"
+  CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/uncompressed/cm-cli_darwin_amd64
+  echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/uncompressed/cm-cli_darwin_amd64${NC}"
+
+  echo -e "${MAGENTA}${ROCKET} Compressing ${BLUE}darwin/amd64${MAGENTA}...${NC}"
+  upx -f --best --lzma ./dist/uncompressed/cm-cli_darwin_amd64 -o ./dist/compressed/cm-cli_darwin_amd64 --force-macos
+  echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/compressed/cm-cli_darwin_amd64${NC}"
+}
+
+function windows_amd64() {
+  echo -e "${MAGENTA}${ROCKET} Building ${BLUE}windows/amd64${MAGENTA}...${NC}"
+  CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X main.GITHUB_TOKEN=$GITHUB_TOKEN" -o ./dist/uncompressed/cm-cli_windows_amd64
+  echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/uncompressed/cm-cli_windows_amd64${NC}"
+
+  echo -e "${MAGENTA}${ROCKET} Compressing ${BLUE}windows/amd64${MAGENTA}...${NC}"
+  upx -f --best --lzma ./dist/uncompressed/cm-cli_windows_amd64 -o ./dist/compressed/cm-cli_windows_amd64
+  echo -e "${GREEN}${CHECKMARK} File saved as ${BLUE}./dist/compressed/cm-cli_windows_amd64${NC}"
+}
+
+if [ "$1" == "linux_amd64" ]; then
+  linux_amd64
+elif [ "$1" == "darwin_amd64" ]; then
+  darwin_amd64
+elif [ "$1" == "windows_amd64" ]; then
+  windows_amd64
+elif [ "$1" != "" ]; then
+  echo -e "${RED}${CROSSMARK} ${BLUE}$1${YELLOW} not supported.${NC}"
+  echo -e "${RED}${CROSSMARK} Nothing to build.${NC}"
+  exit 1
+else
+  linux_amd64
+  darwin_amd64
+  windows_amd64
+fi
