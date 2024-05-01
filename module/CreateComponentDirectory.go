3080056bcd97543bacdff42692b268324a446197
OnCloud <anonymousaaaa41414141@gmail.com>
2024-05-01 16:01:20 +0800

    feat: :sparkles: skip creating component directory if exists

--- module/CreateComponentDirectory.go
+++ module/CreateComponentDirectory.go
@@ -3,15 +3,23 @@ package module
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/gookit/color"
+	"github.com/gookit/config/v2"
 )
 
 func CreateComponentDirectory(projectPath string) (string, error) {
-	path := filepath.Join(projectPath, "lazco_components")
+	COMPONENT_DIRECTORY := config.String("component_directory")
+
+	path := filepath.Join(projectPath, COMPONENT_DIRECTORY)
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err = os.Mkdir(path, 0755)
 		return path, err
+	} else {
+		color.Yellowp("Warning: Component directory ")
+		color.Cyanp(COMPONENT_DIRECTORY)
+		color.Yellowln(" already exists. Using existing directory.")
+		return path, nil
 	}
-
-	return path, nil
 }
