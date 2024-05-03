c2778f52e2ee950de6b911aefeec511d587907b9
OnCloud <anonymousaaaa41414141@gmail.com>
2024-05-03 12:04:13 +0800

    v1.3.3

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		Name:     "Component-Manager",
 		HelpName: "cm",
 		Usage:    "A tool for managing JS/TS components and modules.",
-		Version:  "v1.3.2",
+		Version:  "v1.3.3",
 		Commands: []*cli.Command{
 			{
 				Name:    "version",
